e4cf2972ff83569c3894652c088c99074203d10f
Arunkumar0r7 <arunemy71@gmail.com>
2025-02-26 12:15:14 +0530

    Learned about diff types of methods and paramters

--- src/methodexample.java
+++ src/methodexample.java
@@ -1,5 +1,59 @@
-public class methodexample {
-    public static void main(String[] args) {
-        int a = 1;
-    }
-}
+//public class methodexample {
+////void method with non parameters
+//    public static void add(){
+//        int a = 10;
+//        int b = 20 ;
+//        int c = a+b;
+//        System.out.println(c);
+//    }
+//    public static void main(String[] args) {
+//        add();
+//        System.out.println("A is printed ");
+//    }
+//}
+
+//void method with parameters
+
+//public class methodexample {
+//
+//   public static void add(int a ,int b){
+//       int c= a+b;
+//       System.out.println(c);
+//   }
+//
+//    public static void main(String[] args) {
+//       add(20,10);
+//        System.out.println("A is printed ");
+//    }
+//}
+
+// non void method with non parameters
+
+//public class methodexample {
+//
+//    public static int add(){
+//        int a = 20;
+//        int b = 30;
+//        int c= a+b;
+//        return c;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(add());
+//        System.out.println("Add is printed ");
+//    }
+//}
+
+// non void method with paramters
+
+//public class methodexample {
+//
+//    public static int add(int a,int b,int c){
+//        int d = a+b+c;
+//        return d;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(add(20,30,40));
+//    }
+//}
