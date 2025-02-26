f1aab27dae6a2a4adc9f2f2007ef59be362eadc3
Arunkumar0r7 <arunemy71@gmail.com>
2025-02-26 17:31:34 +0530

    revised accessmodifiers with encapusulation

---
+++ src/Arrayexample.java
@@ -0,0 +1,12 @@
+//public class Arrayexample {
+//    public static void main(String[] args) {
+//
+//
+//        int[] numbers = {10, 20, 30, 40};
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println("Element: " + numbers[i]);
+//        }
+//
+//    }
+//}
