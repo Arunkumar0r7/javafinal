19191e78016670283f77343406603ca62793b208
Arunkumar0r7 <arunemy71@gmail.com>
2025-02-26 17:35:53 +0530

    learned about class and creating the objects

--- src/Objectexample.java
+++ src/Objectexample.java
@@ -1,2 +1,89 @@
-public class Objectexample {
-}
+//public class Objectexample {
+//
+//    void result() {
+//        System.out.println("Result of CIA exams");
+//
+//        int number = 10;
+//        String name = "Arun";
+//        char Grade = 'A';
+//        System.out.println("Roll number:" + number);
+//        System.out.println("Name of the Student: " + name);
+//        System.out.println("Exam Grade is: " +Grade);
+//    }
+//
+//    void display2(){
+//        int number =20;
+//        String name ="karthi";
+//        char grade= 'c';
+//        System.out.println(number);
+//        System.out.println(name);
+//        System.out.println(grade);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Objectexample doc = new Objectexample();
+//        doc.result();
+//        doc.display2();
+//    }
+//}
+
+
+//public class Objectexample {
+//
+//    void result() {
+//        System.out.println("Result of CIA exams");
+//
+//        int number = 10;
+//        String name = "Arun";
+//        char Grade = 'A';
+//        System.out.println("Roll number:" + number);
+//        System.out.println("Name of the Student: " + name);
+//        System.out.println("Exam Grade is: " +Grade);
+//    }
+//
+//
+//    void display2(int number,String name, char grade){
+//        System.out.println("Roll Number" + number);
+//        System.out.println("Name of the Studet:" + name);
+//        System.out.println("Exam Grade is: " + grade);
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Objectexample doc = new Objectexample();
+//        doc.result();
+//        doc.display2(20,"karthi",'B' );
+//    }
+//}
+
+
+// creating objects
+//    class Applform{
+//    String name;
+//    int age;
+//
+//    void details(){
+//        System.out.println(name);
+//        System.out.println(age);
+//    }
+//}
+//
+//public class Objectexample {
+//    public static void main(String[] args) {
+//        Applform obj = new Applform();
+//        obj.name="Arun";
+//        obj.age=25;
+//        obj.details();
+//
+//        Applform obj2 = new Applform();
+//        obj2.name="Karthi";
+//        obj2.age=24;
+//        obj.details();
+//
+//
+//    }
+//}
+
+
