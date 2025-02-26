c95e57992bd43821e6f6436e6ebdf6036a5458a7
Arunkumar0r7 <arunemy71@gmail.com>
2025-02-26 18:42:41 +0530

    revised about Inheritance and the types inheritance and done with methodoveride

---
+++ src/inheritanceexam.java
@@ -0,0 +1,45 @@
+//class animal {
+//
+//    void sound() {
+//        System.out.println("Animal sound");
+//    }
+//}
+//
+//class dog extends animal{
+//
+//}
+//public class inheritanceexam {
+//    public static void main(String[] args) {
+//        dog obj=new dog();
+//        obj.sound();
+//    }
+//}
+
+//class grandfather{
+//
+//    int money = 1000;
+//
+//    void print(){
+//        System.out.println("hlooo i am having money");
+//    }
+//}
+//
+//class father extends grandfather{
+//    @Override
+//    void print() {
+//        System.out.println("hloo i too having money");
+//    }
+//}
+//
+//class son extends grandfather{
+//
+//
+//}
+//
+//public class inheritanceexam {
+//    public static void main(String[] args) {
+//        son obj = new son();
+//        System.out.println("Money : " + obj.money);
+//        obj.print();
+//    }
+//}
