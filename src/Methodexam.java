29822b3131fcfce25116e97c62734b9926772a6a
Arunkumar0r7 <arunemy71@gmail.com>
2025-02-26 17:52:43 +0530

    revised method overloading

---
+++ src/Methodexam.java
@@ -0,0 +1,26 @@
+//method overloading
+public class Methodexam{
+
+    void sum(int a) {
+        System.out.println(a);
+    }
+
+    void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    void sum(int a, int b, int c) {
+        System.out.println(a + b + c);
+    }
+
+    public static void main(String[] args) {
+
+            Methodexam obj= new Methodexam();
+            obj.sum(10);
+            obj.sum(10,20);
+            obj.sum(10,20,30);
+    }
+}
+
+
+
