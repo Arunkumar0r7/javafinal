cb09575cf054f707c20345640ffac60addfcdf41
Arunkumar0r7 <arunemy71@gmail.com>
2025-02-27 09:59:28 +0530

    revised interface and multiple inheritance

---
+++ src/interfaceexam.java
@@ -0,0 +1,51 @@
+//interface father{
+//
+//    abstract void call();
+//    abstract void talk();
+//
+//}
+//    class interfaceexam implements father{
+//
+//    public void call(){
+//        System.out.println("Calling");
+//    }
+//    public void talk(){
+//        System.out.println("talking");
+//    }
+//
+//    public static void main(String[] args) {
+//        interfaceexam ob = new interfaceexam();
+//        ob.call();
+//        ob.talk();
+//    }
+//}
+
+//multiple inheritance
+
+//interface father{
+//
+//    abstract void call();
+//    abstract void talk();
+//
+//}
+//
+//interface mother{
+//    abstract void call();
+//    abstract void talk();
+//}
+//        //son
+//class interfaceexam implements father,mother{
+//
+//    public void call(){
+//        System.out.println("Calling");
+//    }
+//    public void talk(){
+//        System.out.println("talking");
+//    }
+//
+//    public static void main(String[] args) {
+//        interfaceexam ob = new interfaceexam();
+//        ob.call();
+//        ob.talk();
+//    }
+//}
