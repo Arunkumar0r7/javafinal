11b120c994d2a71a35bd2e47aad6685053c34d48
Arunkumar0r7 <arunemy71@gmail.com>
2025-02-27 09:40:22 +0530

    revised about abstract class

---
+++ src/AbstractExam.java
@@ -0,0 +1,30 @@
+//abstract class
+//
+//abstract class computer {
+//
+//    void turnon(){
+//        System.out.println("turning on");
+//    }
+//}
+//    class Hp extends computer{
+//        void turnon(){
+//            System.out.println("turning on 1");
+//        }
+//    }
+//
+//
+//
+//
+//public class Abstractexam {
+//    public static void main(String[] args) {
+//        Hp ob =new Hp();
+//        ob.turnon();
+//    }
+//}
+
+
+//abstract method
+
+
+//
+
